b06157966b8cf1b5fb9ebbae24c66f4efc9be371
thoutuakshaya <thoutuakshaya1@gmail.com>
2024-11-26 23:10:29 +0530

    todo added

--- Tailswind-project/src/App.tsx
+++ Tailswind-project/src/App.tsx
@@ -1,16 +1,15 @@
+import React from 'react';
+import './App.css';
+import TodoApp from './todo';
+import LoremSection from './lorem';
 
-import "./App.css" ;
-
-const App = () => {
+const App: React.FC = () => {
   return (
     <div>
-      <section className="h-screen m-12 flex text-center items-center justify-center bg-purple-950
-      p-12 text-3xl md:p-14  text-white lg:p-16 xl:p-20">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nihil ullam excepturi molestias consequuntur iste aperiam quo! Consectetur quos reprehenderit rerum ratione rem nemo mollitia iusto illum dolorem voluptatibus explicabo ipsa impedit, libero sequi, expedita itaque autem et similique eligendi maiores earum? Officiis accusantium quas magni placeat consequuntur, tempora magnam quis ipsa quia modi eius fuga assumenda impedit voluptatem praesentium?</p>
-      </section>
-{/* //screen changes accoringly with the gadget used */}
+      <TodoApp />
+      <LoremSection />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Tailswind-project/src/todo.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+interface Todo {
+  id: number;
+  text: string;
+}
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task.trim()) {
+      setTodos([...todos, { id: Date.now(), text: task }]);
+      setTask('');
+    }
+  };
+
+  const removeTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold mb-4 text-center">Todo App</h1>
+
+        <div className="flex gap-2 mb-4">
+          <input
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Add a new task"
+            className="flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+          />
+          <button
+            onClick={addTodo}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+          >
+            Add
+          </button>
+        </div>
+
+        <ul className="space-y-2">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex justify-between items-center p-2 bg-gray-100 rounded-lg"
+            >
+              <span>{todo.text}</span>
+              <button
+                onClick={() => removeTodo(todo.id)}
+                className="text-red-500 hover:text-red-700 transition"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
