a2360c44d6cda9ef7ab8019213c574c9a9ba7e61
thoutuakshaya <thoutuakshaya1@gmail.com>
2024-12-10 19:10:09 +0530

    10-12-2024
    catch all routing -'use client' is used.to get the entered route in browser itself.

--- project/src/app/products/users/[...username]/page.tsx
+++ project/src/app/products/users/[...username]/page.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+'use client';
 
-const productList = () => {
+import React from 'react'
+import { usePathname } from 'next/navigation'
+
+
+const ProductList = () => {
+  const pathname = usePathname(); // Full URL path (e.g., /products/users/abcde)
+  const username = pathname?pathname.split('/').pop():'loading'; // Extract username from the path
+
   return (
     <>
-    <h1 >product list</h1>
-    <h2>product 1</h2>
-    <h2>product 2</h2>
-    <h2>product 3</h2>
+      <h1>Hello dynamic routing {username}</h1>
     </>
   )
 }
 
-export default productList
+export default ProductList;
+
+
+
+
+// import React from 'react'
+
+// const productList = ({Params}) => {
+//   return (
+//     <>
+//     {/* <h1 >product list</h1>
+//     <h2>product 1</h2>
+//     <h2>product 2</h2>
+//     <h2>product 3</h2> */}
+//     <h1>hello dynamic routing {Params.username[2]}</h1>
+//     </>
+//   )
+// }
+
+// export default productList
