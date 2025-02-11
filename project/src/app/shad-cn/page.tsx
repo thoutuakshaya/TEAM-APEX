475fb929f3b4269a0fdf558a4130e0e8636c540b
thoutuakshaya <thoutuakshaya1@gmail.com>
2025-02-11 23:35:15 +0530

    11-02-25

--- project/src/app/shad-cn/page.tsx
+++ project/src/app/shad-cn/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import React from 'react';
-import { Button } from '@shadcn/ui'; // Import Button from ShadCN UI
+//import { Button } from '@shadcn/ui'; // Import Button from ShadCN UI
 
 
 const App = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <Button
+      <button
         onClick={() => alert('Button clicked!')}
         className="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
       >
         Click Me
-      </Button>
+      </button>
     </div>
   );
 };
