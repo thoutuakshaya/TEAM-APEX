cbe53c5e0e20ae8ded423f18a3c93fc7f3c5197a
thoutuakshaya <thoutuakshaya1@gmail.com>
2025-02-15 22:49:33 +0530

    created auth-trails
    imported shad-cn,used button.

---
+++ auth-trials/README2.md
@@ -0,0 +1,15 @@
+1.Installed new nextjs application naming auth-trails.
+    npx create-next-app@latest auth-trials
+2.Added shad-cn and initialized.
+     npx shadcn@latest init -d
+     ---Above leads to force installation as Iam using React-19.
+3.Now we can add components after this.
+4.We will see certain changes in out project folder ,
+    --A new folder lib is generated with utils.ts as subfolder.
+    --certain features are added in global.css
+    --components.json also changed.
+5.If still stuck till here ,visit official website----->https://ui.shadcn.com/docs
+6.I loved creating new background colors page with the code in page.tsx
+7.we can change the button usage in page.tsx by changing the button.tsx in components\ui\button.tsx and import it in variant.
+8.Then we created auth folder inside components folder.
+9.
---
+++ auth-trials/app/page.tsx
@@ -0,0 +1,37 @@
+import {Button} from "@/components/ui/button"
+import { Poppins } from "next/font/google";
+import {cn} from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+
+const font=Poppins({  
+  subsets: ['latin'],
+  weight: ["600"]
+})
+export default function Home() {
+    return(<>
+      {/* //i have changed the variant to custom from outline to keep desired layout and added custom in button.tsx
+      <Button size="lg" variant="custom">
+        click me
+      </Button> */}
+
+      <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
+       from-sky-400 to-blue-800">
+        <div className="space-y-6 text-center">
+      <h1 className={cn(
+        "text-6xl drop-shadow-md font-semibold  text-white",
+        font.className
+      )}>
+        🔐AUTH
+      </h1>
+      <p className="text-white">a simple authentication service</p>
+      </div>
+      <div>
+        <LoginButton>
+      <Button variant="secondary" className="w-lg">Sign in</Button>
+      </LoginButton>
+      </div>
+      </main>
+    </>
+    )
+}
